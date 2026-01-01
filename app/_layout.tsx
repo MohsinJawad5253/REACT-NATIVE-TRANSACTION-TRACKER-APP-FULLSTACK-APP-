28c2b44914cb0b042e1b2869e1e49cd3f02303a7
MOHSIN-JAWADWALA <mohsinjawadwala8@gmail.com>
2026-01-01 22:26:29 +0530

    Explicitly pass publishableKey to ClerkProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,15 @@ import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Slot } from 'expo-router'
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
 export default function RootLayout() {
+  if (!publishableKey) {
+    throw new Error("Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env file or app.json extra field.");
+  }
+
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <SafeScreen>
         <Slot />
       </SafeScreen>
